eb0b92543c1e399f04e7006c8d23127b664b5b1b
Thorsten Lorenz <thlorenz@gmx.de>
2015-02-20 15:22:20 -0500

    app: loading the entire polymer

--- app/client/main.js
+++ app/client/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var jsonHeaders = { "Content-Type": "application/json" };
+
 var xhr = require('xhr')
 var traceviewify = require('traceviewify');
 
@@ -8,7 +10,26 @@ var profBtnEl = document.getElementById('cpuprofile-btn');
 
 profBtnEl.onclick = onloadProfile;
 
+function logError(err) {
+  console.error(err);
+}
+
+function onloadedProfile(err, res, body) {
+  if (err) return console.error(err);
+  try {
+    var cpuprofile = JSON.parse(res.body);
+    processCpuProfile(cpuprofile);
+  } catch (e) {
+    logError(new Error('Invalid cpuprofile: ' + e.message));
+  }
+}
+
 function onloadProfile(e) {
   var url = profUrlEl.value;
   console.log('loading profile from', url);
+  xhr({ uri: url, headers: jsonHeaders }, onloadedProfile);
+}
+
+function processCpuProfile(cpuprofile) {
+  var traceview = traceviewify(cpuprofile);
 }
--- app/server.js
+++ app/server.js
@@ -4,6 +4,7 @@ var PORT = 9000;
 var fs = require('fs');
 var path = require('path');
 var http = require('http');
+var root = path.join(__dirname, '..');
 var client = path.join(__dirname, 'client');
 var fixtures = path.join(__dirname, 'fixtures');
 
@@ -24,30 +25,59 @@ function onSIGTERM() {
 
 console.error('pid', process.pid);
 
-function serveError (res, err) {
+function getContentType(url) {
+  var ext = path.extname(url).slice(1);
+  switch(ext) {
+    case 'html' : return 'text/html';
+    case 'css'  : return 'text/css';
+    case 'js'   : return 'application/javascript';
+    case 'json' : return 'application/json';
+    case 'map'  : return 'application/map';
+    default     : console.error('Unknown extension for ' + url); return '';
+  }
+}
+function serveError(res, err) {
   console.error(err);
   res.writeHead(500, { 'Content-Type': 'text/plain' });
   res.end(err.toString());
 }
 
-function serveIndex (res) {
+function serveHtml(res, fullPath) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  fs.createReadStream(path.join(client, 'index.html')).pipe(res); 
+  fs.createReadStream(fullPath).pipe(res); 
 }
 
-function serveBundle (res) {
+function serveJavaScript(res, fullPath) {
   res.writeHead(200, { 'Content-Type': 'application/javascript' });
-  fs.createReadStream(path.join(client, 'bundle.js')).pipe(res); 
+  fs.createReadStream(fullPath).pipe(res); 
 }
 
-function serveCss (res) {
+function serveIndex(res) {
+  serveHtml(res, path.join(client, 'index.html'));
+}
+
+function serveBundle(res) {
+  serveJavaScript(res, path.join(client, 'bundle.js'));
+}
+
+function serveCss(res) {
   res.writeHead(200, { 'Content-Type': 'text/css' });
   fs.createReadStream(path.join(client, 'index.css')).pipe(res); 
 }
 
-function serveCpuProfile (res) {
+function serveCpuProfile(res) {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   fs.createReadStream(path.join(fixtures, 'fibonacci.cpuprofile')).pipe(res); 
+}
+
+function serveComponent(res, url) {
+  res.writeHead(200, { 'Content-Type': getContentType(url) });
+  fs.createReadStream(path.join(root, 'third_party', url)).pipe(res); 
+}
+
+function serveTrace_Viewer(res, url) {
+  res.writeHead(200, { 'Content-Type': getContentType(url) });
+  fs.createReadStream(path.join(root, 'trace_viewer', url)).pipe(res); 
 }
 
 function onRequest(req, res) {
@@ -58,6 +88,20 @@ function onRequest(req, res) {
   if (req.url === '/index.css') return serveCss(res);
   if (req.url === '/cpuprofile') return serveCpuProfile(res);
 
+  // traceviewer/polymer deps
+  if (req.url === '/trace_viewer.html') 
+    return serveHtml(res, path.join(root, 'trace_viewer/trace_viewer.html'));
+  if (req.url === '/gl-matrix-min.js')
+    return serveJavaScript(res, path.join(root, 'third_party', 'gl-matrix', 'dist', 'gl-matrix-min.js'));
+  if (req.url === '/d3.min.js')
+    return serveJavaScript(res, path.join(root, 'third_party', 'd3', 'd3.min.js'));
+  if (req.url === '/jszip.min.js')
+    return serveJavaScript(res, path.join(root, 'third_party', 'jszip', 'jszip.min.js'));
+
+  if (/^\/(base|core|extras)/.test(req.url)) return serveTrace_Viewer(res, req.url);
+  if (/^\/components/.test(req.url)) return serveComponent(res, req.url);
+
+  console.error('Could not find', req.url);
   res.writeHead(404);
   res.end();
 }
