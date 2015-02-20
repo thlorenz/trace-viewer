14174143ebd1e77f8b60a1d0a65b1adca0033386
Thorsten Lorenz <thlorenz@gmx.de>
2015-02-20 16:42:35 -0500

    app: working prototype
    - can load cpuprofile from url
    - simple error handling via alert
    - results of loading cpuprofile are interactive as in chrome://tracing
    - swapped out sample cpuprofile with a nicer one
    - simplified styles since actions are added on left side of header
    - initializing everything on window load since otherwise things weren't
      working properly
    - problems:
      - drag slider not properly working
      - page height seems too large
      - page scrolls to top after every interaction with the sunburst

--- app/client/main.js
+++ app/client/main.js
@@ -5,31 +5,78 @@ var jsonHeaders = { "Content-Type": "application/json" };
 var xhr = require('xhr')
 var traceviewify = require('traceviewify');
 
-var profUrlEl = document.getElementById('cpuprofile-url');
-var profBtnEl = document.getElementById('cpuprofile-btn');
+var profUrlEl, profBtnEl, actionsEl, timelineViewEl;
 
-profBtnEl.onclick = onloadProfile;
+function reportError(err, type) {
+  console.error(type, err);
+  /* global alert*/
+  if (!tv.ui || !tv.ui.Overlay) 
+    return alert(type + '\n  ' + err.toString());
 
-function logError(err) {
-  console.error(err);
+  var overlay = new tv.ui.Overlay();
+  overlay.textContent = tv.normalizeException(err).message;
+  overlay.title = type;
+  overlay.visible = true;
 }
 
 function onloadedProfile(err, res, body) {
-  if (err) return console.error(err);
+  if (err) return reportError(err, 'Loading error');
+  if (res.statusCode !== 200) 
+    return reportError(res.statusCode + ': ' + res.rawRequest.statusText, 'Loading error');
+
+  var cpuprofile;
   try {
-    var cpuprofile = JSON.parse(res.body);
-    processCpuProfile(cpuprofile);
+    cpuprofile = JSON.parse(res.body);
   } catch (e) {
-    logError(new Error('Invalid cpuprofile: ' + e.message));
+    return reportError(new Error('Invalid cpuprofile: ' + e.message), 'JSON parse error');
   }
+  processCpuProfile(cpuprofile);
 }
 
 function onloadProfile(e) {
   var url = profUrlEl.value;
-  console.log('loading profile from', url);
   xhr({ uri: url, headers: jsonHeaders }, onloadedProfile);
 }
 
 function processCpuProfile(cpuprofile) {
-  var traceview = traceviewify(cpuprofile);
+  var trace;
+  try {
+    trace = traceviewify(cpuprofile);
+  } catch(e) {
+    return reportError(e,'Conversion error');
+  }
+  createViewFromTrace(trace);
 }
+
+// functionality adapted from ./examples/trace_viewer.html
+function createViewFromTrace(trace) {
+  /*global tv*/
+  var traces = [ JSON.stringify(trace) ];
+  var m = new tv.c.TraceModel();
+  var p = m.importTracesWithProgressDialog(traces, true);
+  p.then(
+    function onsuccess() {
+      timelineViewEl.model = m;
+      timelineViewEl.tabIndex = 1;
+      if (timelineViewEl.timeline)
+        timelineViewEl.timeline.focusElement = timelineViewEl;
+      timelineViewEl.viewTitle = '';
+    },
+    function onerror(err) {
+      reportError(err, 'Import error');
+    });
+}
+
+function onload() {
+  actionsEl = document.getElementById('actions');
+  profUrlEl = document.getElementById('cpuprofile-url');
+  profBtnEl = document.getElementById('cpuprofile-btn');
+  profBtnEl.onclick = onloadProfile;
+
+  timelineViewEl = document.querySelector('x-timeline-view');
+  tv.b.ui.decorate(timelineViewEl, tv.TraceViewer);
+  timelineViewEl.leftControls.appendChild(actionsEl);
+
+}
+
+window.addEventListener('load', onload);
